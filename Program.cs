22924678eb06178248cbe4e7a24bd8685237d90b
Eriver18 <almeidalopeserivelton@gmail.com>
2024-08-29 00:10:39 -0300

    desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
+Iphone iphone = new Iphone("12421321", "14", "11111111111111", 90);
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
+
+Nokia nokia= new Nokia("1242131", "8", "22222222222222", 500);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
 // TODO: Realizar os testes com as classes Nokia e Iphone
